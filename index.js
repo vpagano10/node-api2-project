8335b3e93567c1396481650539992fd1034320fa
vpagano10 <vpagano10@gmail.com>
2019-12-05 13:34:12 -0500

    made changes to deploy on heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./api/server');
 
-server.listen(4000, () => {
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
     console.log(`\n Server running on localhost:4000 \n`);
 });
